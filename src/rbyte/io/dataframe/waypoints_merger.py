c3844dff7f1d95f7b6056e6f64f3679c129b0be3
Nikita Koritskii <koritcky@gmail.com>
2025-01-30 12:36:11 +0100

    multimodal wpts input

--- src/rbyte/io/dataframe/waypoints_merger.py
+++ src/rbyte/io/dataframe/waypoints_merger.py
@@ -1,4 +1,4 @@
-from typing import final
+from typing import final, Literal
 import os
 import json
 
@@ -8,6 +8,11 @@ from structlog import get_logger
 import shapely
 import geopandas as gpd
 from pyproj import Transformer
+from enum import Enum, auto
+
+
+
+CoordinatesType = Literal["lat_lon", "xy"]
 
 
 logger = get_logger(__name__)
@@ -17,107 +22,127 @@ logger = get_logger(__name__)
 class DataFrameWaypointsMerger:
     __name__ = __qualname__
 
-    ego_lat_col = "Gnss.latitude"
-    ego_lon_col = "Gnss.longitude"
-    wpts_lat_col = "Waypoints.lat"
-    wpts_lon_col = "Waypoints.lon"
     wpts_lat_lon_col = "Waypoints.gnss.lat_lon"
-    heading_col = "heading"
+    heading_col = "heading_rad"
 
     def __init__(
         self,
         waypoints_path: os.PathLike,
-        timestamp_column: str,
+        coordinates_type: CoordinatesType,
+        ego_lat_y_col: str ,
+        ego_lon_x_col: str ,
+        timestamp_col: str,
         num_waypoints: int = 10,
         out_col: str = "Waypoints.xy",
         predict_mode: bool = False,
     ) -> None:
+        """
+        We assume that wpts and ego coordinates are in the same CRS.
+        """
+
+        assert coordinates_type in ["lat_lon", "xy"], "coordinates_type must be either 'lat_lon' or 'xy'"
+        match coordinates_type:
+            # not necessary tho
+            case "lat_lon":
+                self.wpts_lon_x_col = "Waypoints.lon"
+                self.wpts_lat_y_col = "Waypoints.lat"
+            case "xy":
+                self.wpts_lon_x_col = "Waypoints.x"
+                self.wpts_lat_y_col = "Waypoints.y"
+
+        self.ego_lon_x_col = ego_lon_x_col
+        self.ego_lat_y_col = ego_lat_y_col
+
         self.waypoints_path = waypoints_path
-        self.timestamp_column = timestamp_column
-        self.num_waypoints = num_waypoints
+        self.timestamp_col = timestamp_col
+        self.n_wpts = num_waypoints
         self.out_col = out_col
         self.predict_mode = predict_mode
-        self.transformer_to_utm = Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True)
+        self.transform = self._transform_factory(coordinates_type)
+    
+
+    @staticmethod
+    def _transform_factory(coordinates_type: CoordinatesType) -> Transformer:
+        match coordinates_type:
+            case "lat_lon":
+                return Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True).transform
+            case "xy":
+                return lambda *args: args
 
     def __call__(self, input: pl.DataFrame) -> pl.DataFrame:
         logger.debug("Building waypoints dataframe")
         df_wpts = self._build_df_wpts(self.waypoints_path)
-
         logger.debug("Joining waypoints dataframe")
         df = input.join_asof(
             df_wpts.select([
-                "datetime",
-                self.wpts_lat_col,
-                self.wpts_lon_col,
+                self.timestamp_col,
+                self.wpts_lat_y_col,
+                self.wpts_lon_x_col,
                 self.heading_col,
             ]),
-            left_on=self.timestamp_column,
-            right_on="datetime",
+            on=self.timestamp_col,
             strategy="nearest",
-        ).drop("datetime")
+        )
 
         logger.debug("Centering and rotating waypoints dataframe")
         df = self._center_and_rotate(df, out_col=self.out_col)
 
         if self.predict_mode:
+            # Some auxilary columns for rerun visualization
             logger.debug("Building auxilary columns")
             df = df.with_columns(
-                pl.struct([self.wpts_lat_col, self.wpts_lon_col])
+                pl.struct([self.wpts_lat_y_col, self.wpts_lon_x_col])
                 .map_elements(
                     lambda row: list(
-                        zip(row[self.wpts_lat_col], row[self.wpts_lon_col], strict=False)
+                        zip(row[self.wpts_lat_y_col], row[self.wpts_lon_x_col], strict=False)
                     ),
                     return_dtype=pl.List(pl.List(pl.Float64)),
                 )
                 .alias(self.wpts_lat_lon_col)
-            ).drop(self.wpts_lat_col, self.wpts_lon_col)
+            ).drop(self.wpts_lat_y_col, self.wpts_lon_x_col)
 
             df = self.build_heading_triangle(
                 df,
-                l=self._approximate_radius_deg(20),
-                ego_lat_col=self.ego_lat_col,
-                ego_lon_col=self.ego_lon_col,
+                ego_lat_col=self.ego_lat_y_col,
+                ego_lon_col=self.ego_lon_x_col,
                 heading_col=self.heading_col,
             )
         logger.debug("Waypoints merged")
-        return df[:-1] # hack to handle last frame rbyte problem
+        return df[:-1].drop(self.wpts_lat_y_col, self.wpts_lon_x_col) # hack to handle last frame rbyte problem
 
     def _build_df_wpts(self, waypoints_path: os.PathLike) -> pl.DataFrame:
-        waypoints_json = self._read_json(waypoints_path)
-        df_wpts = self._json_to_df(waypoints_json)
-        df_wpts = df_wpts.with_columns(
-            datetime=pl.from_epoch(df_wpts["timestamp"] * 1e9, time_unit="ns")
-        ).sort("datetime")
-        df_wpts = pl.concat([df_wpts] + [df_wpts[-1, :]] * (self.num_waypoints - 1)) # duplicate last waypoints
+        df_wpts = self._json_to_df(waypoints_path)
+        df_wpts = pl.concat([df_wpts] + [df_wpts[-1, :]] * (self.n_wpts - 1)) # duplicate last waypoints
         df_wpts = df_wpts.with_row_index().with_columns(
             index=pl.col("index").cast(pl.Int32)
         )
-        for col in ["lon", "lat"]:
+        for col in [self.wpts_lon_x_col, self.wpts_lat_y_col]:
             df_wpts = (
                 df_wpts.group_by_dynamic(
-                    index_column="index", period=f"{self.num_waypoints}i", every="1i"
+                    index_column="index", period=f"{self.n_wpts}i", every="1i"
                 )
-                .agg(pl.col(col).alias(f"Waypoints.{col}"))
+                .agg(pl.col(col).alias(col))
                 .join(df_wpts, on="index", how="left")
             )
-        return df_wpts[: -(self.num_waypoints - 1)]
+        return df_wpts[: -(self.n_wpts - 1)]
 
-    def _json_to_df(self, json_data: dict) -> pl.DataFrame:
-        data = {"timestamp": [], "heading": [], "lon": [], "lat": []}
-
-        for feature in json_data["features"]:
-            data["timestamp"].append(feature["properties"]["timestamp"])
-            data["heading"].append(radians(feature["properties"]["heading"]))
-            data["lon"].append(feature["geometry"]["coordinates"][0])
-            data["lat"].append(feature["geometry"]["coordinates"][1])
-
-        df_wpts = pl.DataFrame(data)
-        return df_wpts
-
-    def _read_json(self, file_path: os.PathLike) -> dict:
+    def _json_to_df(self, file_path: os.PathLike) -> pl.DataFrame:
         with open(file_path, "r") as file:
-            data = json.load(file)
-        return data
+            geojson = json.load(file)
+
+        data = {self.timestamp_col: [], self.heading_col: [], self.wpts_lon_x_col: [], self.wpts_lat_y_col: []}
+        for feature in geojson["features"]:
+            data[self.timestamp_col].append(feature["properties"]["timestamp"])
+            data[self.heading_col].append(radians(feature["properties"]["heading"]))
+            data[self.wpts_lon_x_col].append(feature["geometry"]["coordinates"][0])
+            data[self.wpts_lat_y_col].append(feature["geometry"]["coordinates"][1])
+        return (
+            pl.DataFrame(data)
+            .with_columns(
+                pl.from_epoch(pl.col(self.timestamp_col) * 1e9, time_unit="ns").alias(self.timestamp_col)
+            )
+            .sort(self.timestamp_col)
+        )
 
     def _center_and_rotate(
         self,
@@ -131,23 +156,23 @@ class DataFrameWaypointsMerger:
         """
 
         # wpts
-        x_wpts, y_wpts = self.transformer_to_utm.transform(
-            df[self.wpts_lon_col].explode(), 
-            df[self.wpts_lat_col].explode()
+        x_wpts, y_wpts = self.transform(
+            df[self.wpts_lon_x_col].explode(), 
+            df[self.wpts_lat_y_col].explode()
         )
         wpts_series = gpd.GeoSeries(shapely.points(x_wpts, y_wpts))
 
         # ego
-        x_ego, y_ego = self.transformer_to_utm.transform(
-            df.select(pl.col(self.ego_lon_col).repeat_by(self.num_waypoints).flatten().explode()), 
-            df.select(pl.col(self.ego_lat_col).repeat_by(self.num_waypoints).flatten().explode())
+        x_ego, y_ego = self.transform(
+            df.select(pl.col(self.ego_lon_x_col).repeat_by(self.n_wpts).flatten().explode()), 
+            df.select(pl.col(self.ego_lat_y_col).repeat_by(self.n_wpts).flatten().explode())
         )
         ego_pos_series = gpd.GeoSeries(shapely.points(x_ego, y_ego)[:, 0])
 
 
         x_diff = wpts_series.x - ego_pos_series.x
         y_diff = wpts_series.y - ego_pos_series.y
-        headings = df.select(pl.col(self.heading_col).repeat_by(self.num_waypoints).flatten().explode())
+        headings = df.select(pl.col(self.heading_col).repeat_by(self.n_wpts).flatten().explode())
 
         df_ = (
             pl.DataFrame({'_x_diff': x_diff, '_y_diff': y_diff, "_heading": headings})
@@ -162,54 +187,21 @@ class DataFrameWaypointsMerger:
             )
             .drop(['_x_diff', '_y_diff', '_heading'])
             .group_by_dynamic(
-                index_column='index', period=f'{self.num_waypoints}i', every=f'{self.num_waypoints}i'
+                index_column='index', period=f'{self.n_wpts}i', every=f'{self.n_wpts}i'
             ).agg(
                 pl.col('xy').list.to_array(width=2).alias(out_col)
             )
             .drop('index')
         )
-        return df.hstack(df_)
-
-    # def _center_and_rotate(self, df: pl.DataFrame, out_col: str) -> pl.DataFrame:
-    #     breakpoint()
-    #     df = df.with_columns(
-    #         pl.struct([self.ego_lat_col, self.ego_lon_col, self.wpts_lat_col, self.wpts_lon_col, self.heading_col])
-    #         .map_elements(
-    #             lambda row: self._delta_xy(
-    #                 row[self.ego_lat_col],
-    #                 row[self.ego_lon_col],
-    #                 row[self.wpts_lat_col],
-    #                 row[self.wpts_lon_col],
-    #                 row[self.heading_col]
-    #             ),
-    #             return_dtype=pl.List(pl.List(pl.Float64)),
-    #         )
-    #         .alias(out_col)
-    #     )
-    #     return df
-    
-    # @staticmethod
-    # def _delta_xy(p1_lat:float, p1_lon:float, p2_lat_list:list[float], p2_lon_list:list[float], heading:float):
-    #     breakpoint()
-    #     def calculate_delta(p2_lat, p2_lon):
-    #         x_delta = geodesic((p1_lat, p1_lon), (p1_lat, p2_lon)).meters
-    #         if p2_lon < p1_lon:
-    #             x_delta = -x_delta
-    #         y_delta = geodesic((p1_lat, p1_lon), (p2_lat, p1_lon)).meters
-    #         if p2_lat < p1_lat:
-    #             y_delta = -y_delta
-    #         return (
-    #             x_delta * cos(heading) - y_delta * sin(heading),
-    #             x_delta * sin(heading) + y_delta * cos(heading)
-    #         )
-
-    #     delta_list = list(map(calculate_delta, p2_lat_list, p2_lon_list))
-    #     return delta_list
-    
+        return df.hstack(df_)    
 
     @staticmethod
     def build_heading_triangle(
-        df: pl.DataFrame, l: float, ego_lat_col: str, ego_lon_col: str, heading_col: str
+        df: pl.DataFrame, 
+        ego_lat_col: str, 
+        ego_lon_col: str, 
+        heading_col: str,
+        l: float = 1.8 * 1e-4 # approx 20 meters
     ) -> pl.DataFrame:
         """
         Build a triangle of points from the ego position and heading.
